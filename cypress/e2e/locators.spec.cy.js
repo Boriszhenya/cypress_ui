51605dd8b0ed02262395f6350bc04a0c54b68235
Borys Pedorenko <boriszhenya@gmail.com>
2024-03-14 12:35:21 +0100

    new

--- cypress/e2e/locators.spec.cy.js
+++ cypress/e2e/locators.spec.cy.js
@@ -1,5 +1,5 @@
 import '@cypress/xpath'; // Официально xpath устарел и больше не поддерживается
-
+///<reference types = "Cypress"/>
 describe('WebDriver University - Locators test', () => {
     beforeEach(() => {
         // Заходим на сайт http://webdriveruniversity.com/ в начале каждого теста
@@ -7,26 +7,64 @@ describe('WebDriver University - Locators test', () => {
     })
 
     it('should be able to find all buttons with CSS', () => {
-        // Нажимаем кнопку
-        cy.get('#button1 > p').click();
-        // // Проверям сообщение
-        cy.contains('Well done for successfully using the click() method!').should('be.visible');
-        // // Закрываем его
-         cy.get('[data-dismiss="modal"]').first().click()
+        
       
-        cy.get('#button2').click({force: true}); //пришлось добавить нехорошую штуку
-        cy.contains('We can use JavaScript code if all else fails! Remember always try to use the WebDriver Library method(s) first such as WebElement.click(). (The Selenium development team have spent allot of time developing WebDriver functions etc).').should('be.visible');
-        cy.get('[data-dismiss="modal"]').eq(3).click()
+      
+        cy.window().then(popupWindow => {
+            cy.get('#button1 > p').click().screenshot();
+             cy.contains('Well done for successfully using the click() method!').should('be.visible');
+            cy.get('[data-dismiss="modal"]').first().click().screenshot();
+            cy.visit('http://webdriveruniversity.com/Click-Buttons/index.html');
+        });
+              
+            
+        cy.window().then(popupWindow => {
+            cy.get('#button2').click(); 
+            cy.contains('We can use JavaScript code if all else fails! Remember always try to use the WebDriver Library method(s) first such as WebElement.click(). (The Selenium development team have spent allot of time developing WebDriver functions etc).').should('be.visible');
+            cy.get('[data-dismiss="modal"]').eq(2).click();
+            cy.visit('http://webdriveruniversity.com/Click-Buttons/index.html');
+        });
 
-        cy.get('#button3').click({force: true}); //пришлось добавить нехорошую штуку
-        cy.get('.modal-title').eq(2).contains('Well done! the Action Move & Click can become very useful!').should('be.visible');
-        cy.get('[data-dismiss="modal"]').eq(5).click()
-
+        cy.window().then(popupWindow => {
+            cy.get('#button3').click().screenshot(); 
+            cy.get('.modal-title').eq(2).contains('Well done! the Action Move & Click can become very useful!').should('be.visible');
+            cy.get('[data-dismiss="modal"]').eq(5).click();
+            cy.visit('http://webdriveruniversity.com/Click-Buttons/index.html');
+        });
 
         cy.get('#main-header > h1').first().then(($header) => {
             console.log("Text from header: " + $header.text());
         });
     });
+
+    it('should open modal, assert text, and close modal', () => {
+        // Нажимаем кнопку чтобы открыть модальное окно
+        cy.get('#button1').click();
+    
+        // Ждём что оно отображается
+        cy.get('#myModalClick > .modal-dialog').should('be.visible');
+    
+        // Переключаемся в него
+        cy.get('#myModalClick > .modal-dialog').within(() => {
+            // Проверяем что у него есть текст
+            cy.contains('Well done for successfully using the click() method!')
+                .should('be.visible');
+    
+            // Внутри его контента ищем кнопку и закрываем
+            cy.get('.modal-content').within(() => {
+               cy.get('.close').click();
+                // cy.contains('Close').click();
+            });
+        });
+
+        // Проверяем что окна нет
+        cy.get('#myModalClick > .modal-dialog').should('not.be.visible');
+    });
+
+
+
+
+
 
     it('should be able to find all buttons with XPATH', () => {
         // Нажимаем кнопку
@@ -35,6 +73,7 @@ describe('WebDriver University - Locators test', () => {
         cy.contains('Well done! the Action Move & Click can become very useful!').should('be.visible');
         // Закрываем его
         cy.xpath('(//button[.="Close"])[3]').click()
-        // И так далее...
+        // И так далее..
+        
     });
 });
